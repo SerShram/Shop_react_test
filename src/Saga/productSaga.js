8c23fb2b5a353fa888abba7a72a1ce45567b2041
Serge <sershram66@gmail.com>
2021-10-03 18:10:44 +0300

    preloading AC saga

--- src/Saga/productSaga.js
+++ src/Saga/productSaga.js
@@ -1,12 +1,14 @@
 import {put, takeEvery, call} from "redux-saga/effects";
-import {FETCH_PRODUCTS, setProductsAC} from "../Redux/catalog-reducer";
+import {FETCH_PRODUCTS, setProductsAC, toggleIsFetchingAC} from "../Redux/catalog-reducer";
 
 const fetchProductsFromApi = () => fetch('https://fakestoreapi.com/products');
 
 function* fetchProductWorker() {
     const data = yield call(fetchProductsFromApi);
     const json = yield call(() => new Promise(res => res(data.json())));
+    yield put(toggleIsFetchingAC(true));
     yield put(setProductsAC(json));
+    yield put(toggleIsFetchingAC(false));
 }
 
 export function* productWatcher() {
